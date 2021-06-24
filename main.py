c0caa507a0f9743a4f7509febacbaa58e61da6d3
konafx <22848261+konafx@users.noreply.github.com>
2021-06-24 20:27:44 +0900

    fix miss

--- main.py
+++ main.py
@@ -32,32 +32,31 @@ async def internal_delete_dispand(bot: discord.Client, message: discord.Message,
     :param operator_id: 削除指示したやつのID
     """
     if message.author.id != bot.user.id:
-        # 削除は自己責任で
         return
 
     embed = message.embeds[0]
 
     if embed.author is None:
         # author が設定されていない埋め込みメッセージは discord が展開した embed と仮定して削除する
-        print("embed.author is None")
+        print("embed.author is None, delete")
         await message.delete()
         return
 
     if getattr(embed.author, 'url', None) is None:
         # author.url が設定されたいない埋め込みメッセージは discord が展開した embed と仮定して削除する
-        print("embed.author.url is None")
+        print("embed.author.url is None, delete")
         await message.delete()
         return
 
     params = get_dispand_info(embed.author.url)
     if params is None:
         # params が取得できない埋め込みメッセージは discord が展開した embed (twitterなど) と仮定して削除する
-        print("dispand info is None")
+        print("dispand info is None, delete")
         await message.delete()
         return
 
-    if operator_id not in params:
-        print("operator is invalid")
+    if operator_id not in params.values():
+        print(f"operator is invalid. {params=} {operator_id=}")
         return
 
     print("operator is valid")
@@ -95,12 +94,13 @@ def main():
                 channel = message.guild.get_channel(message.reference.channel_id)
                 m = await channel.fetch_message(message.reference.message_id)
 
+            print(m)
             if m is None:
                 return
             if m.author.id != client.user.id:
                 return
 
-            await internal_delete_dispand(client, m, message.author)
+            await internal_delete_dispand(client, m, message.author.id)
         else:
             """
             dispand message
@@ -111,7 +111,7 @@ def main():
     async def on_raw_reaction_add(payload):
         await delete_dispand(client, payload=payload)
 
-    print('running...')
+    print("running...")
     client.run(token)
 
 
