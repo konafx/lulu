548698280ced6745bf97cf535f44df84542fac10
konafx <afxlieonon@gmail.com>
2021-06-24 14:47:49 +0900

    mini fix

--- main.py
+++ main.py
@@ -2,24 +2,29 @@ import discord
 from dispander import dispand, delete_dispand
 import os
 
-intents = discord.Intents.none()
-intents.guild_messages = True
-intents.reactions = True
-# message.guild 取得に必要
-intents.guilds = True
+def main():
+    intents = discord.Intents.none()
+    intents.guild_messages = True
+    intents.reactions = True
+    # message.guild 取得に必要
+    intents.guilds = True
 
-client = discord.Client(intents=intents)
+    client = discord.Client(intents=intents)
 
-@client.event
-async def on_message(message):
-    if message.author.bot:
-        return
-    await dispand(message)
+    token = os.environ['DISCORD_BOT_TOKEN']
 
-@client.event
-async def on_raw_reaction_add(payload):
-    await delete_dispand(client, payload=payload)
+    @client.event
+    async def on_message(message):
+        if message.author.bot:
+            return
+        await dispand(message)
 
-print("running...")
-token = os.environ['DISCORD_BOT_TOKEN']
-client.run(token)
+    @client.event
+    async def on_raw_reaction_add(payload):
+        await delete_dispand(client, payload=payload)
+
+    print("running...")
+    client.run(token)
+
+if __name__ == '__main__':
+    main()
